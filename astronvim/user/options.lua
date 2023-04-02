61c168ce1ff53d8fb6b75f1fb5f794481bd3247c
Ilya Sovtsov <ilyasovty@gmail.com>
2023-04-02 17:50:06 +0400

    Nvim options update

--- astronvim/user/options.lua
+++ astronvim/user/options.lua
@@ -1,28 +1,32 @@
 return {
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   opt = {
-    -- set to true or false etc.
-    relativenumber = false,                  -- sets vim.opt.relativenumber
-    number = true,                           -- sets vim.opt.number
-    spell = false,                           -- sets vim.opt.spell
-    signcolumn = "auto",                     -- sets vim.opt.signcolumn to auto
-    wrap = true,                             -- sets vim.opt.wrap
-    conceallevel = 2,                        -- enable conceal
+    smarttab = true,
+    expandtab = true,
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+    relativenumber = true,
+    number = true,
+    spell = false,
+    signcolumn = "auto",
+    wrap = true,
+    conceallevel = 2,
     foldenable = false,
-    foldexpr = "nvim_treesitter#foldexpr()", -- set Treesitter based folding
+    foldexpr = "nvim_treesitter#foldexpr()",
     foldmethod = "expr",
-    linebreak = false,                       -- linebreak soft wrap at words
-    list = false,                            -- show whitespace characters
+    linebreak = false,
+    list = false,
     listchars = { tab = " ", extends = "⟩", precedes = "⟨", trail = "·", eol = "﬋" },
     showbreak = "﬌ ",
   },
   g = {
-    mapleader = " ",                   -- sets vim.g.mapleader
-    autoformat_enabled = true,         -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true,                -- enable completion at start
-    autopairs_enabled = true,          -- enable autopairs at start
-    diagnostics_enabled = true,        -- enable diagnostics at start
-    status_diagnostics_enabled = true, -- enable diagnostics in statusline
-    icons_enabled = true,              -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ",
+    autoformat_enabled = true,
+    cmp_enabled = true,
+    autopairs_enabled = true,
+    diagnostics_enabled = true,
+    status_diagnostics_enabled = true,
+    icons_enabled = true,
   },
 }
