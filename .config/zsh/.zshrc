f692749c47a22395f3b2b0b88b7c1ff7dbc812ee
bo1led-owl <ilyasovty@gmail.com>
2024-07-22 14:39:14 +0400

    Restucturing

---
+++ .config/zsh/.zshrc
@@ -0,0 +1,18 @@
+autoload -U colors && colors
+autoload -Uz vcs_info
+
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats ' %b'
+setopt PROMPT_SUBST
+export PROMPT=' %{$fg[green]%}%~%{$reset_color%}%{$fg[magenta]%}${vcs_info_msg_0_}%{$reset_color%} $ '
+
+alias hx="helix --log /dev/null"
+alias ls="ls --color=auto -h"
+  
+HISTSIZE=5000
+SAVEHIST=5000
+HISTFILE=~/.cache/zsh/history.txt
+
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 2>/dev/null
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
--- update_repo.sh
+++ update_repo.sh
@@ -1,18 +1,24 @@
-cp -r ~/.config/nushell/*.nu ./nushell
-cp -r ~/.config/zsh/ .
-cp -r ~/.config/alacritty .
-cp -r ~/.config/foot .
-cp -r ~/.config/helix .
-cp -r ~/.config/nvim .
-cp -r ~/.config/sway .
-cp -r ~/.config/hypr .
-cp -r ~/.config/wofi .
-cp -r ~/.config/tmux .
-cp -r ~/.config/waybar .
-cp -r ~/.config/fuzzel .
-cp -r ~/.config/mako .
-cp ~/.config/zls.json .
-cp -r ~/.config/btop .
-cp -r ~/.config/neofetch .
+mkdir .config
+mkdir ./.config/nushell
+
+
+cp -r ~/.config/nushell/*.nu ./.config/nushell
+cp -r ~/.config/zsh/ ./.config/
+cp -r ~/.config/alacritty ./.config/
+cp -r ~/.config/foot ./.config/
+cp -r ~/.config/helix ./.config/
+cp -r ~/.config/nvim ./.config/
+cp -r ~/.config/sway ./.config/
+cp -r ~/.config/hypr ./.config/
+cp -r ~/.config/wofi ./.config/
+cp -r ~/.config/tmux ./.config/
+cp -r ~/.config/waybar ./.config/
+cp -r ~/.config/fuzzel ./.config/
+cp -r ~/.config/mako ./.config/
+cp ~/.config/zls.json ./.config/
+cp -r ~/.config/btop ./.config/
+cp -r ~/.config/neofetch ./.config/
+
+cp ~/.zprofile .
 cp -r ~/scripts .
 cp ~/.gdbinit .
